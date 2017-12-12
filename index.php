fd7047c7e20d39d7bd6a26de4e9575dea8e916ed
bobkeri <keribacus@gmail.com>
2017-12-12 04:38:38 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MIT BACUS Broken Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Broken Rock Paper Scissors</h1>
+<h1><b>Workshop 3</b></h1>
+<a href="login.php">Please Log In</a>
+</div>
+</body>
+
